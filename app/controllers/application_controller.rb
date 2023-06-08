f92e68a7f2ac9ada88773f229583f9cd67aeef07
Karen Polanco <polanco.k@gmail.com>
2023-06-08 17:44:12 -0400

    commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -6,5 +8,24 @@ class ApplicationController < Sinatra::Base
     shows = Show.all 
     shows.to_json(include: :genre)
   end
+
+  post "/shows" do
+    show = Show.create(
+      title: params[:title],
+      genre_id: params[:genre_id],
+      seasons: params[:seasons],
+      number_of_episodes: params[:number_of_episodes],
+      original_language: params[:original_language],
+      ongoing: params[:ongoing]
+    )
+    show.to_json
+  end
+
+  delete "/shows/:id" do
+    show = Show.find(params[:id])
+    show.destroy
+    show.to_json
+  end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,6 @@ Show.create(title: "Stargate SG-1", seasons: 10, number_of_episodes: 214, origin
 Show.create(title: "Battlestar Galactica", seasons: 4, number_of_episodes: 76, original_language: "English", ongoing: "F", genre_id: 6 )
 Show.create(title: "The Mandalorian", seasons: 3, number_of_episodes: 24, original_language: "English", ongoing: "T", genre_id: 6 )
 Show.create(title: "The Great British Bake Off", seasons: 13, number_of_episodes: 13, original_language: "English", ongoing: "T", genre_id: 2 )
+Show.create(title: "Casa de Las Flores", seasons: 3, number_of_episodes: 34, original_language: "Spanish", ongoing: "F", genre_id: 8 )
 
 puts "✅ Done seeding!"
