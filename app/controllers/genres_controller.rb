5f466fe05ae476e3bb97a751539843149a97e424
Karen Polanco <polanco.k@gmail.com>
2023-06-20 20:15:32 -0400

    commit message

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,26 +1,16 @@
 class GenresController < ApplicationController
 
-    # def join_tables 
-    #     genre = Genre.find_by(params[:id])
-    #     Show.where(genre_id: genre_id)
-    #    genres_with_shows = Genre.joins(:shows).where(shows: {genre_id: genre_id})
-    # end
-
     get "/genres" do 
         genres = Genre.all
         genres.to_json(include: :shows)
     end
 
-    # get "/genres/:id" do
-    #     genre = Genre.find(params[:id])
-    #     shows = genre.shows
-    #     shows.to_json
-    #   end
 
     post "/genres" do
         genre = Genre.create( 
             name: params[:name]
-        ).to_json
+        )
+        genre.to_json(include: :shows)
     end
 
     patch "/genres/:id" do 
@@ -28,6 +18,11 @@ class GenresController < ApplicationController
         genre.update(
             name: params[:name]
         )
+    end
+
+    delete '/genres/:id' do 
+        genre = Genre.find(params[:id])
+        genre.destroy
     end
 
 
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -6,36 +6,42 @@ class ShowsController < ApplicationController
         shows = Show.all
         shows.to_json(include: :genre)
     end
+    
+    get "/shows/:id" do
+      show = Show.find(params[:id])
+      show.to_json
+    end
 
-    post '/shows' do
-      show = Show.create(
+    post 'genres/:genre_id/shows' do
+      genre = Genre.find(params[:genre_id])
+      binding.pry
+      show = genre.shows.Create(
         title: params[:title],
-        # genre: params[:genre],
         seasons: params[:seasons],
         episodes: params[:episodes],
         language: params[:language],
         ongoing: params[:ongoing],
       )
       show.to_json
-      
     end
 
     patch '/shows/:id' do 
       show = Show.find(params[:id])
       show.update(
         title: params[:title],
-        genre_id: params[:genre_id],
+       
         seasons: params[:seasons],
         episodes: params[:episodes],
         language: params[:language],
         ongoing: params[:ongoing],
-      ).to_json
+      )
+      show.to_json
     end
 
     delete "/shows/:id" do
       show = Show.find(params[:id])
       show.destroy
-      show.to_json
+
     end
 
 end
