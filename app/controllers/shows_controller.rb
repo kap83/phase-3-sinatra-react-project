540e65c5b24fb9b64e7724dec70c953056b780f3
Karen Polanco <polanco.k@gmail.com>
2023-06-25 20:48:18 -0400

    commit message

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -12,30 +12,7 @@ class ShowsController < ApplicationController
       show.to_json
     end
 
-    post '/genres/:genre_id/shows' do
-      genre = Genre.find(params[:genre_id])
-      show = genre.shows.create(
-        title: params[:title],
-        seasons: params[:seasons],
-        episodes: params[:episodes],
-        language: params[:language],
-        ongoing: params[:ongoing],
-      )
-      show.to_json(include: :genre)
-    end
 
-    patch '/genres/:genre_id/shows/:id' do 
-      genre = Genre.find(params[:genre_id])
-      show = genre.shows.find(params[:id])
-      show.update(
-        title: params[:title],
-        seasons: params[:seasons],
-        episodes: params[:episodes],
-        language: params[:language],
-        ongoing: params[:ongoing],
-      )
-      show.to_json(include: :genre)
-    end
 
     delete "/shows/:id" do
       show = Show.find(params[:id])
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class Genre < ActiveRecord::Base
-    has_many :shows
+    has_many :shows, dependent: :destroy
 
 end
