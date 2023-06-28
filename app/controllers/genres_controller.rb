ec144ada6610195ee26837a9cb16209539b10a85
Karen Polanco <polanco.k@gmail.com>
2023-06-28 15:14:43 -0400

    commit message

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,40 +5,17 @@ class GenresController < ApplicationController
         genres.to_json(include: :shows)
     end
 
-    get "genres/:id" do
-        genre = Genre.find(params[:id])
-        genre.to_json(include: :shows)
+
+    post '/genre' do
+      genre = Genre.create(
+        name: params[:name]
+      )
+      genre.to_json
     end
 
-    post '/genres/:genre_id/shows' do
-        genre = Genre.find(params[:genre_id])
-        show = genre.shows.create(
-          title: params[:title],
-          seasons: params[:seasons],
-          episodes: params[:episodes],
-          language: params[:language],
-          ongoing: params[:ongoing],
-        )
-        show.to_json
-      end
-  
-    patch '/genres/:genre_id/shows/:id' do 
-        genre = Genre.find(params[:genre_id])
-        show = genre.shows.find(params[:id])
-        show.update(
-          title: params[:title],
-          seasons: params[:seasons],
-          episodes: params[:episodes],
-          language: params[:language],
-          ongoing: params[:ongoing],
-        )
-        show.to_json
-      end
-
+   
       delete "/genres/:id" do
         genre = Genre.find(params[:id])
         genre.destroy
-        #if line 44 is not included, when deleting i get the following error => unexpected run time errors: unexpected end of JSON input, syntaxError: Unexpected end of JSON input
-        genre.to_json  
       end
 end
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -12,13 +12,34 @@ class ShowsController < ApplicationController
       show.to_json
     end
 
+    post '/genres/:genre_id/shows' do
+      genre = Genre.find(params[:genre_id])
+      show = genre.shows.create(
+        title: params[:title],
+        seasons: params[:seasons],
+        episodes: params[:episodes],
+        language: params[:language],
+        ongoing: params[:ongoing],
+      )
+      show.to_json
+    end
 
+  patch '/genres/:genre_id/shows/:id' do 
+      genre = Genre.find(params[:genre_id])
+      show = genre.shows.find(params[:id])
+      show.update(
+        title: params[:title],
+        seasons: params[:seasons],
+        episodes: params[:episodes],
+        language: params[:language],
+        ongoing: params[:ongoing],
+      )
+      show.to_json
+    end
 
-    delete "/shows/:id" do
+  delete "/shows/:id" do
       show = Show.find(params[:id])
       show.destroy
-      #if line 44 is not included, when deleting i get the following error => unexpected run time errors: unexpected end of JSON input, syntaxError: Unexpected end of JSON input
-      show.to_json
     end
 
 end
